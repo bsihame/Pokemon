2f86fe607ffd113ec8360b102e9f765415650fcb
bsihame <sihamebazi@pursuit.org>
2021-01-23 21:08:27 -0500

    add  code to app js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Pokemon from "./components/Pokemon";
+// import TypeOfPokemon from "./components/TypeOfPokemon";
+
+import SelectedPokemon from "./components/SelectedPokemon";
+import Error from "./components/Error";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<NavBar />
+			<Switch>
+				<Route exact path={"/"}>
+					<Home />
+				</Route>
+				<Route exact path={"/pokemon"}>
+					<Pokemon />
+					{/* <TypeOfPokemon /> */}
+				</Route>
+				<Route path={"/pokemon/:name"}>
+					<SelectedPokemon />
+				</Route>
+				<Route
+					path="/github"
+					component={() => {
+						window.location.href = "https://github.com/bsihame";
+						return null;
+					}}
+				></Route>
+				<Route
+					path="/linkedin"
+					component={() => {
+						window.location.href = "https://linkedin.com";
+						return null;
+					}}
+				></Route>
+				<Route>
+					<Error />
+				</Route>
+			</Switch>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
